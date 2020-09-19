8b00ff48832b34f096968d46122949109f533f28
Ward <ward@bothrs.com>
2020-09-19 18:20:59 +0200

    Update postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production'
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: ['./pages/**/*.tsx', './components/**/*.tsx'],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
-  },
+  plugins: [
+    'tailwindcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+  ],
 }
